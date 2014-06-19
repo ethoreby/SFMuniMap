13164c5941c528a8acb66d278b7eec6849777d1a
Elliott Thoreby <ethoreby@gmail.com>
2014-06-18 20:32:15 -0700

    rendering neiborhoods

--- public/app.js
+++ public/app.js
@@ -4,8 +4,23 @@ var width = 800;
 var svg = d3.select('body').append('svg')
   .attr({width: width, height: height});
 
-d3.json('arteries.json', function(err, data) {
+
+var projection = d3.geo.mercator()
+    .scale(1000)
+    .translate([width, height]);
+
+var path = d3.geo.path()
+   // .projection(projection);
+
+
+
+d3.json('neighborhoods.json', function(err, data) {
   if(err) return console.log(err);
 
-  
+  console.log(data);
+
+  svg.append("path")
+    .datum(data)
+    .attr("d", path)
+    .style("fill", "red");
 });
