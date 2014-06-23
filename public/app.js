918254c0716dcf47ff33d8f0766ffeabc0539c42
Elliott Thoreby <ethoreby@gmail.com>
2014-06-23 16:39:03 -0700

    control asyc drawing

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 var height = 600;
 var width = 800;
 
-var svg = d3.select('body').append('svg')
+var svg = d3.select('.display').append('svg')
   .attr({width: width, height: height});
 
 svg.append('rect')
@@ -18,34 +18,48 @@ var projection = d3.geo.albers()
 var path = d3.geo.path()
   .projection(projection);
 
-var drawCity = function(pathType) {
-  
-  var reference = {
-    'neighborhoods': '#999999',
-    'streets': 'green',
-    'arteries': 'blue',
-    'freeways': 'red'
-  }
-
-  d3.json(pathType + '.json', function(err, data) {
-  if(err) return console.log(err);
-
-  svg.append('path')
-    .datum(data)
-    .attr('d', path)
-    .style('fill', reference[pathType]);
-  });
-}
-
-drawCity('neighborhoods');
-drawCity('streets');
-drawCity('arteries');
-drawCity('freeways');
 
 var app = angular.module('app', []);
 
 app.controller('MainController', function($scope, $q) {
-  
+
+  var drawCity = function(pathType) {
+    
+    var reference = {
+      'neighborhoods': '#999999',
+      'streets': 'green',
+      'arteries': 'blue',
+      'freeways': 'red'
+    }
+    var dfd = $q.defer();
+
+    d3.json(pathType + '.json', function(err, data) {
+    if(err) return console.log(err);
+
+    svg.append('path')
+      .datum(data)
+      .attr('d', path)
+      .style('fill', reference[pathType]);
+    });
+
+    dfd.resolve();
+    return dfd.promise;
+  }
+
+  drawCity('neighborhoods')
+  .then(function() {
+    console.log('streets');
+    return drawCity('streets');
+  })
+  .then(function() {
+    console.log('artery');
+    return drawCity('arteries')
+  })
+  .then(function() {
+    console.log('freeway');
+    return drawCity('freeways');  
+  })
+
 });
 
 
