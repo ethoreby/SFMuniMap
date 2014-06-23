928f63829f68981bb6649511ccf292bf447d10b4
Elliott Thoreby <ethoreby@gmail.com>
2014-06-23 16:27:13 -0700

    include angular

--- public/app.js
+++ public/app.js
@@ -10,13 +10,13 @@ svg.append('rect')
   .attr('fill', '#66CCFF');
 
 var projection = d3.geo.albers()
-    .center([0, 37.768544])
-    .rotate([122.427521, 0])
-    .scale(250000)
-    .translate([width / 2, height / 2]);
+  .center([0, 37.768544])
+  .rotate([122.427521, 0])
+  .scale(250000)
+  .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
-    .projection(projection);
+  .projection(projection);
 
 var drawCity = function(pathType) {
   
@@ -42,7 +42,11 @@ drawCity('streets');
 drawCity('arteries');
 drawCity('freeways');
 
+var app = angular.module('app', []);
 
+app.controller('MainController', function($scope, $q) {
+  
+});
 
 
 
